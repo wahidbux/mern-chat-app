97b11b782fe749b902aa119d082ebfc8a3a13fe9
WAHID BUX K22SW <wahidbux>
2025-10-02 17:58:41 +0500

    add signup, login, and logout controllers and also routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signupUser = (req,res)=>{
+    res.send("This is signup page.................")
+}
+
+export const loginUser = (req,res)=>{
+    console.log("This is user login page")
+}
+
+export const logoutUser = (req,res)=>{
+    console.log("This is user logout page")
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
 
 const app = express();
+dotenv.config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 app.get('/',(req ,res)=>{
     res.send("Hello Everyone...............")
 })
+
+app.use("/api/auth", authRoutes);
+
 app.listen(port, () => {
-  console.log("Server is running on port 4000");
+  console.log("Server is running on port 8000");
 });
